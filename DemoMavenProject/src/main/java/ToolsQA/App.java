2a62db76306d8394cd6ee312970bbf8fac349902
Jørgen Stamnes <jorghs@stud.ntnu.no>
2019-11-13 11:33:07 +0000

    asd

---
+++ DemoMavenProject/src/main/java/ToolsQA/App.java
@@ -0,0 +1,13 @@
+package DemoMavenProject.src.main.java.ToolsQA;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
